b6fbb32758b707b3a539441aff04a8415d7897ea
Ace200404 <muntadheralmaeeni@gmail.com>
2026-09-24 11:39:11 -0500

    ci: add Go CI workflow with golangci-lint and 80% coverage gate and the plan md file

---
+++ backend/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestAddistion (t*testing.T){
+	result:=add(1,1)
+	expected:=2
+	if result != expected {
+		t.Errorf("Expected %d, but got %d", expected, result)
+	}
+
+}
