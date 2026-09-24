66ef50ea704b793caef608d9f461d603956afd7c
Ace200404 <muntadheralmaeeni@gmail.com>
2026-09-24 12:09:26 -0500

    fix: correct YAML indentation in go-ci workflow

--- backend/main_test.go
+++ backend/main_test.go
@@ -2,11 +2,15 @@ package main
 
 import "testing"
 
-func TestAddistion (t*testing.T){
-	result:=add(1,1)
-	expected:=2
+func TestAddistion(t *testing.T) {
+	result := add(1, 1)
+	expected := 2
 	if result != expected {
 		t.Errorf("Expected %d, but got %d", expected, result)
 	}
 
+}
+
+func add(i1, i2 int) any {
+	panic("unimplemented")
 }
